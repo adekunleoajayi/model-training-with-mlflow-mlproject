d52e845f0393fe60e513029ade427427b4375dc3
Adekunle Ajayi <adeajayi.kunle@gmail.com>
2023-04-07 10:07:23 +0200

    replace sqlite database with csv in datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -1,21 +1,9 @@
-import sqlite3
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def get_train_test_datasets():
-    # Create your connection.
-    database = sqlite3.connect("./data/database.sqlite")
-
-    # - get player_Attributes from the database
-    df = pd.read_sql_query("SELECT * FROM Player_Attributes", database)
-
-    # Remove player_fifa_api_id, player_api_id, date and other columns with integer values
-    df = df.select_dtypes(include=["float"])
-
-    # - remove missing values
-    df = df.dropna()
+    df = pd.read_csv("./data/european_soccer_player.csv")
 
     x = df[df.columns[1:]]
     y = df[df.columns[0]]
